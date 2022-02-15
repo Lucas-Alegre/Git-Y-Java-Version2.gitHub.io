ec42fab9b7c270b95cbb75954729da11f659846b
Lucas Alegre <lucasealegre123@gmail.com>
2022-02-15 17:23:26 -0300

    Version Usuario

---
+++ src/Git/Usuario.java
@@ -0,0 +1,28 @@
+package Git;
+
+public class Usuario {
+
+	private String apellido;
+	private String nombre;
+	
+	public Usuario(String apellido, String nombre) {
+		this.apellido = apellido;
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario ( " + getApellido() + ", " + getNombre() + "]";
+	}
+	
+	
+	
+	
+}
